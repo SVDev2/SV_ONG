a3568937dc025a51586dc58769b2ca473f66fd3b
Dennis (SVDev2) <alexandrefranciele925@gmail.com>
2026-06-26 12:07:54 -0300

    Fiz deversas pequenas melhorias

--- app.py
+++ app.py
@@ -16,55 +16,47 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return render_template("home.html")
+    return render_template("pages/home.html")
     
 @app.route("/sobre")
 def sobre():
-    return render_template("sobre.html")
+    return render_template("pages/sobre.html")
     
 @app.route("/entrar")
 def entrar():
-    return render_template("entrar.html")
+    return render_template("auth/entrar.html")
     
 @app.route("/cadastrar")
 def cadastrar():
-    return render_template("cadastrar.html")
+    return render_template("auth/cadastrar.html")
     
 @app.route("/termos")
 def termos():
-    return render_template("termos.html")
+    return render_template("pages/termos.html")
     
 @app.route("/doacao")
 def doacao():
-    return render_template("doacao.html")
+    return render_template("pages/doacao.html")
     
 @app.route("/configuracoes")
 def configuracoes():
-    return render_template("configuracoes.html")
-    
-@app.route("/rotas")
-def rotas():
-    return render_template("")
-    
-@app.route("/cookies")
-def cookies():
-    return render_template("")
+    return render_template("settings/configuracoes.html")
     
 @app.route("/banner")
 def banner():
-    return render_template("banner_cookies.html")
+    return render_template("footer/banner_cookies.html")
     
 @app.route("/redefinir-senha")
 def recuparaS():
-    return render_template("recupera_senha.html")
+    return render_template("auth/recupera_senha.html")
     
-@app.route("/propagandas-slide")
+@app.route("/slide")
 def slide():
-    return render_template("propagandas_slide.html")
+    return render_template("footer/slide.html")
     
 @app.route("/desenvolvimento")
 def desenvolvimento():
-    return render_template("")
+    return render_template("includes/msg_desenvolvimento")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=4000)
